c3427ca93059b91fef3b53318564d4d25d99e429
jay <wngml0708@gmail.com>
2025-03-28 14:50:23 +0900

    user join

--- src/main/java/com/metacoding/storev2/store/StoreController.java
+++ src/main/java/com/metacoding/storev2/store/StoreController.java
@@ -1,4 +1,16 @@
 package com.metacoding.storev2.store;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
 public class StoreController {
+    private final StoreService storeService;
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
 }
--- src/main/java/com/metacoding/storev2/user/UserService.java
+++ src/main/java/com/metacoding/storev2/user/UserService.java
@@ -2,9 +2,22 @@ package com.metacoding.storev2.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
     private final UserRepository userRepository;
+
+    @Transactional
+    public void 회원가입(UserRequest.JoinDTO joinDTO) {
+        // 1. 동일한 username이 있는지 검사
+        User user = userRepository.findByUsername(joinDTO.getUsername());
+        // 2. 있으면, exception
+        if (user != null) {
+            throw new RuntimeException("동일한 username이 존재 합니다.");
+        }
+        // 3. 없으면 회원가입하기
+        userRepository.save(joinDTO.getUsername(), joinDTO.getPassword(), joinDTO.getFullname());
+    }
 }
